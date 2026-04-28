ee4ab8f574b8d7c3106b7105ec46cb959ed14292
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-28 14:02:18 +0200

    correction, use foreach cicle

--- index.php
+++ index.php
@@ -10,18 +10,6 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-
-<div class="container mt-5 p-5">
-<form action="./filtro.php" class="form-control" method="GET">
-    <input class="form-check-input" type="checkbox" value="" id="checkDefault" name="parking">
-  <label class="form-check-label" for="checkDefault">
-    Default checkbox
-  </label>
-  <br>
-  <button type="submit" class="btn btn-primary">invia</button>
-</form>
-</div>
-
    
 <?php 
 
@@ -65,76 +53,38 @@
 
     ];
 
-    // foreach($hotels as $hotel) {
-
-    //     foreach($hotel as $key => $value) {
-
-    //         echo "$key : $value   ";
-
-    //     };
-
-    //     echo "<br>";
-
-    // };
-
-    echo '<div class="container p-5">
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Descrizione</th>
-      <th scope="col">Parcheggio</th>
-      <th scope="col">Voto</th>
-      <th scope="col">Distanza dal centro</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>'. $hotels[0]['name'] .'</td>
-      <td>'. $hotels[0]['description'] .'</td>
-      <td>'. ($hotels[0]['parking'] === true ? 'Si' : 'No') .'</td>
-      <td>'. $hotels[0]['vote'] .'</td>
-      <td>'. $hotels[0]['distance_to_center'] . ' km' . '</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>'. $hotels[1]['name'] .'</td>
-      <td>'. $hotels[1]['description'] .'</td>
-      <td>'. ($hotels[1]['parking'] === true ? 'Si' : 'No') .'</td>
-      <td>'. $hotels[1]['vote'] .'</td>
-      <td>'. $hotels[1]['distance_to_center'] . ' km' . '</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>'. $hotels[2]['name'] .'</td>
-      <td>'. $hotels[2]['description'] .'</td>
-      <td>'. ($hotels[2]['parking'] === true ? 'Si' : 'No') .'</td>
-      <td>'. $hotels[2]['vote'] .'</td>
-      <td>'. $hotels[2]['distance_to_center'] . ' km' . '</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>'. $hotels[3]['name'] .'</td>
-      <td>'. $hotels[3]['description'] .'</td>
-      <td>'. ($hotels[3]['parking'] === true ? 'Si' : 'No') .'</td>
-      <td>'. $hotels[3]['vote'] .'</td>
-      <td>'. $hotels[3]['distance_to_center'] . ' km' . '</td>
-    </tr>
-    <tr>
-      <th scope="row">5</th>
-      <td>'. $hotels[4]['name'] .'</td>
-      <td>'. $hotels[4]['description'] .'</td>
-      <td>'. ($hotels[4]['parking'] === true ? 'Si' : 'No' ).'</td>
-      <td>'. $hotels[4]['vote'] .'</td>
-      <td>'. $hotels[4]['distance_to_center'] . ' km' . '</td>
-    </tr>
-  </tbody>
-</table>
-</div>';
-
 ?>
+<div class="container">
+<table class="table">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Descrizione</th>
+            <th>Parcheggio</th>
+            <th>Voto</th>
+            <th>Distanza dal centro</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+        foreach($hotels as $hotel) {
+
+        ?>
+        <tr>
+            <td><?php echo $hotel['name'] ?></td>
+            <td><?php echo $hotel['description'] ?></td>
+            <td><?php echo $hotel['parking'] === true ? 'Si' : 'No' ?></td>
+            <td><?php echo $hotel['vote'] ?></td>
+            <td><?php echo $hotel['distance_to_center'] ?></td>
+        </tr>
+
+        <?php
+       } 
+       ?>
+    </tbody>
+</table>
+</div>
 
 </body>
 </html>
